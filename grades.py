a531715dd48dbda87488313aa1c42ce9bb22b798
Kaiden Keane <kjkeane88@gmail.com>
2024-07-14 14:52:53 -0600

    fixed loading whoops

--- grades.py
+++ grades.py
@@ -1,26 +1,29 @@
 def load_data():
+    courses = []
+    
     try:
         with open(save_filename, "r") as f:
-            courses = []
             course_count = 0
             for line in f:
-                if not line.startswith("\t"):
+                if not (line.startswith("\t") or line.startswith(" ")):
                     courses.append(Course(line.strip()))
                     course_count += 1
                     
                 else:
-                    name, data = line[1:].split(": ")
-                    grade, weight = data.split(" ")
-                    courses[course_count - 1].grades.append(Grade(name, float(grade.strip()), float(weight.strip()) ))
-
-        return courses
+                    name, data = line.strip().split(": ")
+                    grade, weight = data.strip().split(" ")
+                    courses[course_count - 1].grades.append( Grade(name, float(grade.strip()), float(weight.strip()) ) )
+    
     except FileNotFoundError:
         print(f"cannot find file {save_filename}")
     except IsADirectoryError:
         print(f"{save_filename} is a directory")
     except PermissionError:
         print(f"insufficient permissions for {save_filename}")
-        
+    
+    return courses
+
+      
 def save_data(courses):
     try:
         with open(save_filename, "w") as f:
@@ -39,13 +42,21 @@ def save_data(courses):
         print(f"{save_filename} is a directory")
     except PermissionError:
         print(f"insufficient permissions for {save_filename}")
-        
 
 
 class Course():
     def __init__(self, name) -> None:
         self.name = name
         self.grades = []
+        self.adjusted_avg = 0
+    
+    def calc_adjusted_weighted_avg(self):
+        total_weighted_score = 0
+        total_weights = 0
+        for grade in self.grades:
+            total_weighted_score += grade.score * grade.weight
+            total_weights += grade.weight
+        self.adjusted_avg = total_weighted_score / total_weights
 
     def __str__(self) -> str:
         return self.name
@@ -53,11 +64,15 @@ class Course():
     def __lt__(self, other):
         return self.name < other.name
 
+
 class Grade():
     def __init__(self, name, score=-1, weight=-1) -> None:
         self.name = name
         self.score = score
         self.weight = weight
+
+    def change_name(self, new_name):
+        self.name = new_name
 
     def change_score(self, new_score):
         self.score = new_score
@@ -67,12 +82,13 @@ class Grade():
     
     def __str__(self) -> str:
         return f"{self.name}:\tgrade: {self.score}\tweight: {self.weight}"
-    
+
+
 def main():
     # load data
     courses = load_data()
-    for course in courses:
-        print(course, end=" ")
+    # for course in courses:
+    #     course.calc_adjusted_weighted_avg()
     print("\n")
 
 
@@ -96,7 +112,7 @@ def main():
         elif choice == "2": # select course
             print("\ncurrent course:")
             for i in range(len(courses)):
-                print(f"\t{i+1}. {courses[i]}")
+                print(f"\t{i+1}. {courses[i]} | avg: {courses[i].adjusted_avg}")
             chosen_num = input(f"select class number 1-{len(courses)}: ")
             chosen_class = courses[int(chosen_num) - 1]
             
@@ -122,15 +138,20 @@ def main():
                     print(f"grades for {chosen_class}")
                     for i in range(len(chosen_class.grades)):
                         print(f"\t{i+1}. {chosen_class.grades[i]}")
+                    
                     chosen_grades_num = input(f"select grade number 1-{len(courses)}: ")
                     chosen_grade = chosen_class.grades[int(chosen_grades_num) - 1]
+                    
                     print(chosen_grade)
+                    
                     print(f"current name: {chosen_grade.name}")
-                    chosen_grade.name = input("new name: ")
+                    chosen_grade.change_name(input("new name: "))
+                    
                     print(f"current grade: {chosen_grade.score}")
-                    chosen_grade.score = float(input("new grade: "))
+                    chosen_grade.change_score(float(input("new grade: ")))
+                    
                     print(f"current weight: {chosen_grade.weight}")
-                    chosen_grade.weight = float(input("new weight: "))
+                    chosen_grade.change_weight(float(input("new weight: ")))
 
         elif choice == "3": # add course
             name = input("name of new course: ")
