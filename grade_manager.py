c09b18502ce25553f0996b8fbc67bfde0169dfcd
Kaiden Keane <kjkeane88@gmail.com>
2024-07-28 20:16:06 -0600

    basic basic stuff

--- grade_manager.py
+++ grade_manager.py
@@ -1,4 +1,16 @@
 import tkinter as tk
+from os import listdir
+from os.path import isfile, join
+from os import mkdir
+
+def foo(event):
+    print("button pressed")
+
+def bind_children_to_click(widget, callback, button="<Button-1>"):
+    """Bind all children of a widget to the click event."""
+    widget.bind("<Button-1>", callback)
+    for child in widget.winfo_children():
+        bind_children_to_click(child, callback, button)
 
 # main window
 class App(tk.Tk):
@@ -9,37 +21,121 @@ class App(tk.Tk):
         self.geometry(f"{dimensions[0]}x{dimensions[1]}")
         self.minsize(dimensions[0], dimensions[1])
 
-        
-
-        
-        
-
+        sem_list = SemesterList(self)
+        sem_list.pack(expand=True, fill='both')
+        print("gfdsf")
         self.mainloop()
 
 
 class SemesterList(tk.Frame):
     def __init__(self, parent) -> None:
-        super().__init__(parent)
-        lb = tk.Listbox(parent)
-        lb.insert(1, "hello")
-        lb.pack()
+        super().__init__(parent, highlightbackground="black", highlightthickness=1)
+        a = tk.Label(self, text="Semesters", font=("Arial", 25), highlightbackground="black", highlightthickness=1)
+        a.pack(side="top")
+        semList = tk.Frame(self, width=100, height=100, highlightbackground="black", highlightthickness=1)
+        
+        self.sem_list = [Semester(semList, "semester 1"), Semester(semList, "semester 2")]
 
+        for sem in self.sem_list:
+            sem.pack(side="top", pady=10)
+        
+        semList.pack(fill=None, expand=False)
+        self.focus()
+        
+
+    def load_semester_data(self, data_directory):
+        semesters = []
+        try:
+            files = [f for f in listdir(data_directory) if isfile(join(data_directory, f))]
+        except FileNotFoundError:
+            mkdir(data_directory)
+            files = [f for f in listdir(data_directory) if isfile(join(data_directory, f))]
+        
+        for file in files:
+            new_sem = Semester(file.split(".")[0]) # dont include the .txt
+            new_sem.courses = self.load_semester(join(data_directory, file))
+            semesters.append(new_sem)
+        
+        return semesters
+
+    def load_semester(self, filename):
+        courses = []
+        
+        try:
+            with open(filename, "r") as f:
+                course_count = 0
+                for line in f:
+                    if not (line.startswith("\t") or line.startswith(" ")):
+                        courses.append(Course(line.strip()))
+                        course_count += 1
+                        
+                    else:
+                        name, data = line.strip().split(": ")
+                        grade, weight = data.strip().split(" ")
+                        courses[course_count - 1].grades.append( Grade(name, float(grade.strip()), float(weight.strip()) ) )
+            print(f"successfully loaded {filename}")
+        
+        except FileNotFoundError:
+            print(f"cannot find file {filename}")
+        except IsADirectoryError:
+            print(f"{filename} is a directory")
+        except PermissionError:
+            print(f"insufficient permissions for {filename}")
+        finally:
+            return courses
+    
+    def save_sem_data(self, data_directory, semesters):
+        for semester in semesters:
+            self.save_semester(data_directory + "/" + semester.name + ".txt", semester.courses)
+    
+    def save_semester(filename, courses):
+        try:
+            with open(filename, "w") as f:
+                for course in courses:
+                    f.write(course.name)
+                    f.write("\n")
+                    
+                    for grade in course.grades:
+                        f.write("\t" + grade.name + ": " + str(grade.score) + " " + str(grade.weight) + "\n")
+            print(f"successfully saved to {filename}")
+                    
+        except FileNotFoundError:
+            print(f"cannot find file {filename}")
+        except IsADirectoryError:
+            print(f"{filename} is a directory")
+        except PermissionError:
+            print(f"insufficient permissions for {filename}")
+            
 
 class Semester(tk.Frame):
-    def __init__(self, parent) -> None:
-        super().__init__(parent)
+    def __init__(self, parent, name) -> None:
+        super().__init__(parent, highlightbackground="black", highlightthickness=1)
+        self.name = name
+        self.courses = []
+        self.avg = 0
+        
+        sem_name = tk.Label(self, text=self.name)
+        sem_grade = tk.Label(self, text=self.avg)
+
+        sem_name.pack(side="left")
+        sem_grade.pack(side="right")
+        
+
+        bind_children_to_click(self, self.foo)
+        self.config(cursor="openhand")
+    
+    def foo(self, event):
+        print(self.name + " has been pressed")
+
 
 
 class CourseList(tk.Frame):
     def __init__(self, parent) -> None:
         super().__init__(parent)
-        tk.Label(self, background="grey").pack(expand=True, fill="both")
-        self.place(x=parent.winfo_width(), y=0, relwidth=0.7, relheight=1)
-
         self.courses = []
 
 
-class Course():
+class Course(tk.Frame):
     def __init__(self, master, name) -> None:
         self.name = name
         frame = tk.Frame(master)
@@ -57,4 +153,4 @@ class Grade(tk.Frame):
 
 
 if __name__ == "__main__":
-    App("grade manager", (600, 600))
+    App("test", (600, 600))
