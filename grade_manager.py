257e9aec379824093697342a07f0c170f7420379
Kaiden Keane <kjkeane88@gmail.com>
2024-07-31 01:36:23 -0600

    mockup done

--- grade_manager.py
+++ grade_manager.py
@@ -33,7 +33,7 @@ class App(tk.Tk):
 
 
 class GUI_SemesterList(tk.Frame):
-    def __init__(self, parent, controller, container): 
+    def __init__(self, parent: tk.Frame, controller: tk.Tk, container: tk.Frame): 
         tk.Frame.__init__(self, container)
         self.grid(row=0, column=0, sticky="nsew")
          
@@ -54,7 +54,7 @@ class GUI_SemesterList(tk.Frame):
 
 
 class GUI_Semester(tk.Frame):
-    def __init__(self, parent, controller, container, list_container, name, average):
+    def __init__(self, parent: GUI_SemesterList, controller: tk.Tk, container: tk.Frame, list_container: tk.Frame, name: str, average: float):
         tk.Frame.__init__(self, list_container, highlightbackground="black", highlightthickness=1)
         self.name = name
         self.avg = average
@@ -77,7 +77,7 @@ class GUI_Semester(tk.Frame):
 
 
 class GUI_CourseList(tk.Frame):
-    def __init__(self, parent, controller, container):
+    def __init__(self, parent: GUI_Semester, controller: tk.Tk, container: tk.Frame):
         tk.Frame.__init__(self, container)
         self.grid(row=0, column=0, sticky="nsew")
         
@@ -86,8 +86,8 @@ class GUI_CourseList(tk.Frame):
         back_btn = tk.Button(self, text="back", command=lambda : controller.show_frame(parent.screen))
         
         self.courseList = tk.Frame(self)
-        self.GUI_course_list = [GUI_Course(self, controller, container, self.courseList, "course 1", 20),
-                             GUI_Course(self, controller, container, self.courseList, "course 2", 20)]
+        self.GUI_course_list = [GUI_Course(self, controller, container, self.courseList, "course 1", 97.3234),
+                             GUI_Course(self, controller, container, self.courseList, "course 2", 67.3)]
 
         # pack children
         title.pack(side="top")
@@ -101,7 +101,7 @@ class GUI_CourseList(tk.Frame):
 
 
 class GUI_Course(tk.Frame):
-    def __init__(self, parent, controller, container, list_container, name, average):
+    def __init__(self, parent: GUI_CourseList, controller: tk.Tk, container: tk.Frame, list_container: tk.Frame, name: str, average: float):
         tk.Frame.__init__(self, list_container, highlightbackground="black", highlightthickness=1)
         self.name = name
         self.avg = average
@@ -124,16 +124,43 @@ class GUI_Course(tk.Frame):
 
 
 class GUI_GradeList(tk.Frame):
-    def __init__(self, parent, controller, container):
+    def __init__(self, parent: GUI_Course, controller: tk.Tk, container: tk.Frame):
         tk.Frame.__init__(self, container)
         self.grid(row=0, column=0, sticky="nsew")
         
         title = tk.Label(self, text=parent.name, font=TITLEFONT)
         back_btn = tk.Button(self, text="back", command=lambda : controller.show_frame(parent.screen))
 
+        self.gradeList = tk.Frame(self)
+        self.GUI_grade_list = [GUI_Grade(self, controller, container, self.gradeList, "grade 1", 34.3),
+                             GUI_Grade(self, controller, container, self.gradeList, "grade 2", 53.0)]
+
+        # pack children
         title.pack(side="top")
         back_btn.pack()
+        for grade in self.GUI_grade_list:
+            grade.pack(side="top", pady=10)
+        
+        # pack self
+        self.gradeList.pack(fill=None, expand=False)
+
   
+
+
+class GUI_Grade(tk.Frame):
+    def __init__(self, parent: GUI_CourseList, controller: tk.Tk, container: tk.Frame, list_container: tk.Frame, name: str, average: float):
+        tk.Frame.__init__(self, list_container, highlightbackground="black", highlightthickness=1)
+        self.name = name
+        self.avg = average
+        self.screen = parent
+
+        course_name = tk.Label(self, text=self.name, font=ITEMNAMEFONT, padx=10)
+        course_grade = tk.Label(self, text=f"avg: {round(self.avg, 2)}", font=ITEMDATAFONT, padx=10)
+
+        course_name.pack(side="left")
+        course_grade.pack(side="right")
+
+
 
 
 if __name__ == "__main__":
